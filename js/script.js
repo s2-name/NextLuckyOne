04208880a075838b0010d3815d6b1730eaa6e188
S2 <80480605+s2-name@users.noreply.github.com>
2021-07-04 23:58:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var allElement = document.getElementById('all');
+var lastnames = document.querySelectorAll('.lastnames');
+
+allElement.addEventListener('click', allOffOn);
+document.getElementById('find').addEventListener('click', findLuckyOne);
+lastnames.forEach(element => {
+    element.addEventListener('click', updateAll);
+});
+
+
+function allOffOn(){
+    if (allElement.checked){
+        lastnames.forEach(function(element){
+            element.checked = true;
+        })
+    }else{
+        lastnames.forEach(function(element){
+            element.checked = false;
+        })
+    }
+}
+function findLuckyOne(){
+    var riskGroup = [];
+    lastnames.forEach(function(lastname){
+        if(lastname.checked == true){
+            riskGroup.push(lastname);
+        }
+    });
+    var arrLen = riskGroup.length;
+    var randomNumber = parseInt(Math.random() * (arrLen - 0) + 0);
+    document.getElementById('result').innerHTML = "Счастливчик: " + riskGroup[randomNumber].getAttribute('data-lastname');
+}
+
+function updateAll(){
+    var isChecked = true;
+    lastnames.forEach(function(element){
+       if(element.checked != true){
+           isChecked = false;
+       }
+    })
+    if (isChecked){
+        allElement.checked = true;
+       }else{
+        allElement.checked = false;
+       }
+}
